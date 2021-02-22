2bfa7fda48c319549364ef1c96b3b03bbd495ade
ignik86 <ignik86@gmail.com>
2021-02-22 15:11:41 +0300

    Optimize number of query

--- log_to_db.py
+++ log_to_db.py
@@ -1,17 +1,19 @@
 import psutil
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 from sqlalchemy import create_engine
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, DateTime, Float
 from sqlalchemy import orm
 from sqlalchemy.sql import func
 
-DB_CONNECT ='sqlite:///test_db.db'
-#DB_CONNECT ='sqlite:///:memory:'
+DB_CONNECT = 'sqlite:///db3.db'
+
+
+# DB_CONNECT ='sqlite:///:memory:'
 
 
 class Values(object):
-    def __init__(self, param_id, value,timestamp):
+    def __init__(self, param_id, value, timestamp):
         self.param_id = param_id
         self.value = value
         self.timestamp = timestamp
@@ -29,79 +31,79 @@ class Params(object):
 
 
 class Db_log:
-  def __init__(self, db_connector):
-    """
+    def __init__(self, db_connector):
+        """
     Create connecton to DB 
     Check if required table exist, if not then create tables params and values. 
     """
-    
-    self.engine = create_engine(db_connector, echo=True)
-    metadata = MetaData(bind=self.engine, reflect=True)
-     
-    if not self.engine.dialect.has_table(self.engine, 'params'):  # If table don't exist, Create.  
-      params_table = Table('params', metadata,
-        Column('id', Integer, primary_key=True),
-        Column('name', String),
-      )
-      metadata.create_all(self.engine) 
 
-    if not self.engine.dialect.has_table(self.engine, 'values'):# If table don't exist, Create.
-      values_table = Table('values', metadata,
-        Column('id', Integer, primary_key=True),
-        Column('param_id', Integer, ForeignKey('params.id')),
-        Column('value', Float), 
-        Column('timestamp', DateTime( timezone=True), default=func.now()),                
-      )
-      metadata.create_all(self.engine)  
+        self.engine = create_engine(db_connector, echo=False)
+        metadata = MetaData(bind=self.engine, reflect=True)
 
-    orm.Mapper(Values, metadata.tables['values'])
-    orm.Mapper(Params, metadata.tables['params'])
-    self.session = orm.Session(bind=self.engine)
+        if not self.engine.dialect.has_table(self.engine, 'params'):  # If table don't exist, Create.
+            params_table = Table('params', metadata,
+                                 Column('id', Integer, primary_key=True),
+                                 Column('name', String),
+                                 )
+            metadata.create_all(self.engine)
 
-  def write_value(self, parametr_name, value):
-    """
+        if not self.engine.dialect.has_table(self.engine, 'values'):  # If table don't exist, Create.
+            values_table = Table('values', metadata,
+                                 Column('id', Integer, primary_key=True),
+                                 Column('param_id', Integer, ForeignKey('params.id')),
+                                 Column('value', Float),
+                                 Column('timestamp', DateTime(timezone=True), default=func.now()),
+                                 )
+            metadata.create_all(self.engine)
+
+        orm.Mapper(Values, metadata.tables['values'])
+        orm.Mapper(Params, metadata.tables['params'])
+        self.session = orm.Session(bind=self.engine)
+
+    def write_value(self, parametr_name, value):
+        """
     Write value to values tables with parameter name
     """
-    # check if  params exist if not then create
-    q = self.session.query(Params).filter(Params.name == parametr_name)
-    record = q.all()
-    if len(record) == 0:
-      param = Params(parametr_name)
-      self.session.add(param)
-      self.session.commit()
-    # get  params id
-    q = self.session.query(Params).filter(Params.name == parametr_name)
-    record = q.all()
-    params_id = record[0].id
-    # write to db
-    val = Values(params_id, value, datetime.utcnow())
-    self.session.add(val)
-    self.session.commit()
-    self.session.close()
+        # check if  params exist if not then create
+        q = self.session.query(Params).filter(Params.name == parametr_name)
+        record = q.all()
+        if len(record) == 0:
+            param = Params(parametr_name)
+            self.session.add(param)
+            self.session.commit()
+        # get  params id
+        q = self.session.query(Params).filter(Params.name == parametr_name)
+        record = q.all()
+        params_id = record[0].id
+        # write to db
+        val = Values(params_id, value, datetime.now())
+        self.session.add(val)
+        self.session.commit()
+        # self.session.close()
 
-  def read_value(self, parametr_id, from_date, to_date):
+    def read_value(self, parametr_id, from_date, to_date):
 
-    q = self.session.query(Values)\
-        .filter(Values.param_id == parametr_id)\
-        .filter(Values.timestamp <= to_date)\
-        .filter(Values.timestamp >= from_date)
-     
-    return q.all()
-  
-  def take_params(self):
-    q = self.session.query(Params)
-    return q.all() 
-  
+        q = self.session.query(Values) \
+            .filter(Values.param_id == parametr_id) \
+            .filter(Values.timestamp <= to_date) \
+            .filter(Values.timestamp >= from_date)
+
+        return q.all()
+
+    def take_params(self):
+        q = self.session.query(Params)
+        return q.all()
+
+
 def main():
-  
-  logger = Db_log(DB_CONNECT)
+    logger = Db_log(DB_CONNECT)
 
-  while True:
-    cpu = psutil.cpu_percent(interval=5)
-    logger.write_value('CPU', cpu)
-    memory = psutil.virtual_memory()
-    logger.write_value('Memory', memory.percent)
-    time.sleep(5)
+    while True:
+        cpu = psutil.cpu_percent(interval=5)
+        logger.write_value('CPU', cpu)
+        memory = psutil.virtual_memory()
+        logger.write_value('Memory', memory.percent)
+        time.sleep(5)
 
 
 if __name__ == '__main__':
--- web_app.py
+++ web_app.py
@@ -10,12 +10,12 @@ from flask_nav import Nav
 from flask_nav.elements import *
 
 from flask import Flask, render_template, redirect, request, url_for
-from log_to_db import Values,Params,Db_log
+from log_to_db import Db_log
 from threading import Thread
 import psutil
 import time
 
-DB_CONNECT ='sqlite:///db3.db'
+DB_CONNECT = 'sqlite:///db3.db'
 nav = Nav()
 SECRET_KEY = os.urandom(32)
 
@@ -26,81 +26,87 @@ def mynavbar():
         'Test App',
         View('Home', 'main'),
         View('Values', 'values'),
-       
+
     )
 
 
 class SelectTag(FlaskForm):
-    
     tag = SelectField(u'Параметр', validators=[DataRequired()])
 
-    
+
 class Logging_to_db(Thread):
+    """
+    Thread for write values in DB
+    """
+
     def run(self):
         while True:
-          cpu = psutil.cpu_percent(interval=5)
-          logger.write_value('CPU', cpu)
-          memory = psutil.virtual_memory()
-          logger.write_value('Memory', memory.percent)
-          logger.session.close()
-          
+            cpu = psutil.cpu_percent(interval=5)
+            memory = psutil.virtual_memory()
+            try:
+                logger.write_value('CPU', cpu)
+                logger.write_value('Memory', memory.percent)
+            except:
+                pass
+            logger.session.close()
+
 
 def create_app():
+    application = Flask(__name__)
 
-  application = Flask(__name__)
+    nav.init_app(application)
+    Bootstrap(application)
+    application.config['SECRET_KEY'] = SECRET_KEY
 
-  nav.init_app(application)
-  Bootstrap(application)
-  application.config['SECRET_KEY'] = SECRET_KEY
-
-  return application
+    return application
 
 
 app = create_app()
 logger = Db_log(DB_CONNECT)
 
 
-@app.route('/values',  methods=('GET', 'POST'))
+@app.route('/values', methods=('GET', 'POST'))
 def values():
     # take all params in params table
-    params = logger.take_params() 
-    
+    params = logger.take_params()
+
     select_form = SelectTag()
-    select_form.tag.choices = [(g.id, g.name) for g in params] # send all params  in params table to select form
-    
+    select_form.tag.choices = [(g.id, g.name) for g in params]  # send all params  in params table to select form
+    current_time = datetime.now()
     # take last hour values  for selected params 
-    values = logger.read_value(select_form.tag.data,
-                               datetime.utcnow() - timedelta(hours=1), 
-                               datetime.utcnow()) # send last hour values to values.html
-    
+    records = logger.read_value(select_form.tag.data,
+                                current_time - timedelta(hours=1),
+                                current_time)  # send last hour values to values.html
     # calculate average values 
-    delta1=60
-    delta2=55
+    delta1 = 60
+    delta2 = 55
     average_values = {}
     show_average = False
-    while delta2 >0:
-      # take records in 5 min interval
-      record = logger.read_value(select_form.tag.data,
-                               datetime.utcnow() - timedelta(minutes=delta1), 
-                               datetime.utcnow() - timedelta(minutes=delta2)) 
-      if len(record) !=0: # check if we have values in interval
-        # calculate average
-        sum = 0
-        for i in range(len(record)):
-          sum += record[i].value
-        timestamp = datetime.utcnow()- timedelta(minutes=delta2-2.5)
-        average_values[timestamp] = sum/len(record) # send to values.html
-        show_average = True
-      # next interval
-      delta1 -=5
-      delta2 -=5
-      
+    while delta2 >= 0:
+        average_records = []
+        start_interval = current_time - timedelta(minutes=delta1)
+        end_interval = current_time - timedelta(minutes=delta2)
+        for record in records:
+            if (record.timestamp >= start_interval) and (record.timestamp <= end_interval):
+                average_records.append(record)
+        if len(average_records) != 0:  # check if we have values in interval
+            # calculate average
+            values_sum = 0
+            for i in range(len(average_records)):
+                values_sum += average_records[i].value
+            timestamp = start_interval + timedelta(minutes=2.5)
+            average_values[timestamp] = values_sum / len(average_records)  # send to values.html
+            show_average = True
+        # next interval
+        delta1 -= 5
+        delta2 -= 5
+
     logger.session.close()
 
-    return render_template('values.html', 
-                           values=values, 
-                           tags=params, 
-                           select=select_form, 
+    return render_template('values.html',
+                           values=records,
+                           tags=params,
+                           select=select_form,
                            average_values=average_values,
                            show_average=show_average)
 
@@ -110,7 +116,6 @@ def main():
     return render_template('index.html')
 
 
-if __name__ == "__main__": 
-  
+if __name__ == "__main__":
     Logging_to_db().start()
-    app.run(host='127.0.0.1', port=5000, debug= False)
+    app.run(host='127.0.0.1', port=5000, debug=False)
